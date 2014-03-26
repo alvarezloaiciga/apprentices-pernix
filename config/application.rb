a25c1bd74898d76baf1cb633e217b4ffc17f8352
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2014-03-25 21:42:07 -0500

    Initial Commit

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require File.expand_path('../boot', __FILE__)
+
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module ApprenticesPernix
+  class Application < Rails::Application
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApprenticesPernix::Application.config.secret_key_base = '51da6824c49f178014a1a47b90a353d54050efe47314c5fe527079bf18f2f5f2e1426b010e7c6dc8d6ab87bc0fd1c10ba6f8e8ff6099c26d866db222173b6664'
