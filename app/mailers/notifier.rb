53af523e395074735d8c1e32823e2ffd12ec514c
Erick Castillo <erickcsh@gmail.com>
2014-03-28 16:53:49 -0600

    Changed default from in notifier class

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default to: AdminUser.pluck(:email), from: ENV["GMAIL_USERNAME"]
+  default to: AdminUser.pluck(:email), from: 'no-reply@pernix-solutions.com' #ENV['GMAIL_USERNAME']
 
   def applicant_apply(applicant)
     @applicant = applicant
