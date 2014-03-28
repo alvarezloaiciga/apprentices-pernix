5eb4db2b25bb8684edbdf705c106251a750c3de4
Erick Castillo <erickcsh@gmail.com>
2014-03-28 10:09:42 -0600

    Config assets precompile remove

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module ApprenticesPernix
   class Application < Rails::Application
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.serve_static_assets = true
-    config.assets.initialize_on_precompile = false
     config.assets.compile = true
     config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js print.css)
     config.assets.enabled = true
